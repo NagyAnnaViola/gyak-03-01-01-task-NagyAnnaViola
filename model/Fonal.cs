f6a65688a035a565a246be9bb7fe58c945c8144b
anna <anna@DESKTOP-VNJJ5K7>
2021-05-15 10:52:36 +0200

    try  catch

--- model/Fonal.cs
+++ model/Fonal.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,9 +19,22 @@ namespace gyak_03_01_01_task_NagyAnnaViola.model
         /// <param name="piece">darabszám</param>
         public Fonal(string type, int piece)
         {
+            try
+            {
+                if (piece == 1) //egy darab fonal
+                    throw new Exception("Egy fonalból nem lehet megfelelő méretű horgolt kendőt, illetve amigurumi fiugrát készíteni, " +
+                        "kérem tegyen még a kosárába fonalat.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Debug.WriteLine(e.Message);
+            }
             this.type = type;
             this.piece = piece;
         }
+
+        
     }
 
 
