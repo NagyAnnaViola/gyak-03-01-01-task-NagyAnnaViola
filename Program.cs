7ab76ea92748126e7a433488cd87e310b93c8ac6
anna <anna@DESKTOP-VNJJ5K7>
2021-05-15 10:41:42 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gyak_03_01_01_task_NagyAnnaViola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ model/Fonal.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gyak_03_01_01_task_NagyAnnaViola.model
+{
+    class Fonal
+    {
+    }
+}
